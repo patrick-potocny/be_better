db04280813216b9f9b1c46818fd770f0df2e08f2
patrick-potocny <patrick.potocny85@gmail.com>
2023-01-28 20:07:05 +0100

    Bug fixes

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,11 @@
-import { Configuration, OpenAIApi } from 'openai';
+import { Configuration, OpenAIApi } from "openai";
 
+const key1 = "sk-6qqzHWK1s";
+const key2 = "JnGzoYkAihtT3BlbkF";
+const key3 = "J2O5Znhb2MQFvhPERWnUY";
 // Init OpenAi instance
 const config = new Configuration({
-  apiKey: process.env.REACT_APP_API_KEY,
+  apiKey: key1 + key2 + key3,
 });
 const openai = new OpenAIApi(config);
 
@@ -14,13 +17,17 @@ async function getBotResponse(userMessage, lastMessage) {
       max_tokens: 3000,
       model: "text-davinci-003",
     });
-    
+
     // Removes any newline characters or other non letters/numbers from the start of the response
-    const responseText = response.data.choices[0].text.replace(/^[^a-zA-Z0-9]+/g, '');
-    if (responseText.trim() === '') return "Even though I am Artificial Intelligence, I am not intelligent enough to respond to that. \n Try to ask me in another way."
-    return responseText
+    const responseText = response.data.choices[0].text.replace(
+      /^[^a-zA-Z0-9]+/g,
+      ""
+    );
+    if (responseText.trim() === "")
+      return "Even though I am Artificial Intelligence, I am not intelligent enough to respond to that. \n Try to ask me in another way.";
+    return responseText;
   } catch (e) {
-    return 'Something went wrong. Try to ask again.'
+    return "Something went wrong. Try to ask again.";
   }
 }
 
