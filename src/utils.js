06e0836c0435f9e2696bfbe5df74b63ee03521cc
patrick-potocny <patrick.potocny85@gmail.com>
2023-01-28 20:11:47 +0100

    Bug fixes

--- src/utils.js
+++ src/utils.js
@@ -1,33 +1,25 @@
-import { Configuration, OpenAIApi } from "openai";
+import axios from 'axios'
 
-const key1 = "sk-6qqzHWK1s";
-const key2 = "JnGzoYkAihtT3BlbkF";
-const key3 = "J2O5Znhb2MQFvhPERWnUY";
-// Init OpenAi instance
-const config = new Configuration({
-  apiKey: key1 + key2 + key3,
-});
-const openai = new OpenAIApi(config);
 
 async function getBotResponse(userMessage, lastMessage) {
   try {
-    const response = await openai.createCompletion({
+    const response = await axios.post('https://api.openai.com/v1/completions', {
       prompt: lastMessage.user + lastMessage.bot + userMessage,
       temperature: 0.5,
       max_tokens: 3000,
       model: "text-davinci-003",
+    }, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${process.env.REACT_APP_API_KEY}`
+      }
     });
-
     // Removes any newline characters or other non letters/numbers from the start of the response
-    const responseText = response.data.choices[0].text.replace(
-      /^[^a-zA-Z0-9]+/g,
-      ""
-    );
-    if (responseText.trim() === "")
-      return "Even though I am Artificial Intelligence, I am not intelligent enough to respond to that. \n Try to ask me in another way.";
+    const responseText = response.data.choices[0].text.replace(/^[^a-zA-Z0-9]+/g, '');
+    if (responseText.trim() === '') return "Even though I am Artificial Intelligence, I am not intelligent enough to respond to that. \n Try to ask me in another way."
     return responseText;
   } catch (e) {
-    return "Something went wrong. Try to ask again.";
+    return 'Something went wrong. Try to ask again.'
   }
 }
 
